1947c162eaa74a3899d0e272609f7bf47e8da42b
nickhendo <50039828+nickhendo@users.noreply.github.com>
2025-04-24 08:52:20 +1000

    Add initial working POC

--- main.go
+++ main.go
@@ -1,22 +1,159 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"log/slog"
+	"net/http"
 	"os/exec"
 	"strings"
 )
 
+func main() {
+	handler := GetRouter()
+	server := http.Server{
+		Addr:    "0.0.0.0:1323",
+		Handler: handler,
+	}
+
+	slog.Info("Listening on port: 1323")
+	err := server.ListenAndServe()
+	if err != nil {
+		slog.Error("Server closed", "error", err)
+	}
+}
+
+type errorHandler func(http.ResponseWriter, *http.Request) error
+
+func (fn errorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := fn(w, r)
+	if err != nil {
+		slog.Error("Unexpected error in ServeHTTP", "error", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func GetRouter() *http.ServeMux {
+	router := http.NewServeMux()
+	router.Handle("GET /repos/test/{repositoryName}/info/refs", errorHandler(GetRepoHandler))
+	router.Handle("POST /repos/test/{repositoryName}/{service}", errorHandler(PostServiceHandler))
+	return router
+}
+
+func PostServiceHandler(writer http.ResponseWriter, request *http.Request) error {
+	service := request.PathValue("service")
+	repositoryName := request.PathValue("repositoryName")
+	repoLocation := "repositories"
+	repoPath := fmt.Sprintf("%s/%s", repoLocation, repositoryName)
+
+	var cmd *exec.Cmd
+	logWriter := LogWriter{writer}
+
+	switch service {
+	case "git-upload-pack":
+		writer.Header().Set("Content-Type", "application/x-git-upload-pack-result")
+		cmd = exec.Command("git", "upload-pack", "--stateless-rpc", "--strict", repoPath)
+	case "git-receive-pack":
+		writer.Header().Set("Content-Type", "application/x-git-receive-pack-result")
+		writer.WriteHeader(http.StatusOK)
+		cmd = exec.Command("git", "receive-pack", "--stateless-rpc", repoPath)
+	default:
+		return fmt.Errorf("unexpected service: %s", service)
+	}
+
+	cmd.Stdin = request.Body
+	cmd.Stdout = logWriter
+
+	var stdErr strings.Builder
+	cmd.Stderr = &stdErr
+
+	err := cmd.Run()
+	if err != nil {
+		log.Printf("%s error: %v, stderr: %s", service, err, stdErr.String())
+	}
+
+	writer.WriteHeader(http.StatusOK)
+	return err
+}
+
+func GetRepoHandler(writer http.ResponseWriter, request *http.Request) error {
+	fmt.Printf("request.Method: %v\n", request.Method)
+	repositoryName := request.PathValue("repositoryName")
+	service := request.URL.Query().Get("service")
+	repoLocation := "repositories"
+	repoPath := fmt.Sprintf("%s/%s", repoLocation, repositoryName)
+
+	var cmd *exec.Cmd
+
+	customWriter := LogWriter{writer}
+
+	switch service {
+	case "git-upload-pack":
+		writer.Header().Set("Content-Type", "application/x-git-upload-pack-advertisement")
+		cmd = exec.Command("git", "upload-pack", "--stateless-rpc", "--advertise-refs", repoPath)
+	case "git-receive-pack":
+		writer.Header().Set("Content-Type", "application/x-git-receive-pack-advertisement")
+		cmd = exec.Command("git", "receive-pack", "--stateless-rpc", "--advertise-refs", repoPath)
+	default:
+		return fmt.Errorf("unexpected service: %s", service)
+	}
+
+	// Write the "# service=git-upload-pack" header in pkt-line format
+	fmt.Fprintf(customWriter, "%04x# service=%s\n", len("# service="+service)+5, service)
+	customWriter.Write([]byte("0000"))
+
+	cmd.Stdout = customWriter
+
+	var stdErr strings.Builder
+	cmd.Stderr = &stdErr
+
+	err := cmd.Run()
+	if err != nil {
+		log.Printf("%s error: %v, stderr: %s", service, err, stdErr.String())
+	}
+
+	writer.WriteHeader(http.StatusOK)
+	return err
+}
+
 func CreateRepo(repoName string) error {
+	if !strings.HasSuffix(repoName, ".git") {
+		return fmt.Errorf("bare repository name must end with '.git'")
+	}
+
+	if strings.Contains(repoName, " ") {
+		return fmt.Errorf("bare repository name must not contain spaces")
+	}
 	defaultBranchName := "main"
 	repoLocation := "repositories"
 
-	input := strings.Join([]string{repoLocation, repoName}, "/")
-	log.Println(input)
-	command := exec.Command("git", "init", "-b", defaultBranchName, input)
+	repoPath := strings.Join([]string{repoLocation, repoName}, "/")
+	slog.Debug("creating repository at", "path", repoPath)
+	command := exec.Command("git", "init", repoPath, "--bare")
+	slog.Debug("command.Args", "value", strings.Join(command.Args, ","))
+
 	var output strings.Builder
 	command.Stdout = &output
 	err := command.Run()
-	log.Println(output.String())
+	slog.Debug(output.String())
+	if err != nil {
+		return err
+	}
+
+	command2 := exec.Command(
+		"git",
+		"symbolic-ref",
+		"HEAD",
+		fmt.Sprintf("refs/heads/%s", defaultBranchName),
+	)
+	command2.Dir = repoPath
+	slog.Debug("command2", "value", command2.String())
+	var output2 strings.Builder
+	command2.Stdout = &output2
+	err = command2.Run()
+	slog.Debug("output2", "value", output2.String())
+
 	return err
 }
 
@@ -24,18 +161,20 @@ func DeleteRepo(repoName string) error {
 	repoLocation := "repositories"
 
 	input := strings.Join([]string{repoLocation, repoName}, "/")
-	log.Println(input)
+	slog.Debug("attempting to delete repository", "path", input)
 	command := exec.Command("rm", "-rf", input)
 	var output strings.Builder
 	command.Stdout = &output
 	err := command.Run()
-	log.Println(output.String())
+	slog.Debug("command", "output", output.String())
 	return err
 }
 
-func main() {
-	err := CreateRepo("test_repo")
-	if err != nil {
-		log.Fatal(err)
-	}
+type LogWriter struct {
+	ResponseWriter http.ResponseWriter
+}
+
+func (w LogWriter) Write(p []byte) (n int, err error) {
+	log.Println("writing: ", string(p))
+	return w.ResponseWriter.Write(p)
 }
---
+++ main_test.go
@@ -0,0 +1,216 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http/httptest"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+type Git struct {
+	remoteName string
+	cloneUrl   string
+
+	tracePacket bool
+	trace       bool
+	curlVerbose bool
+}
+
+func (g Git) Command(name string, arg ...string) (*exec.Cmd, *strings.Builder, *strings.Builder) {
+	command := exec.Command(name, arg...)
+
+	if g.tracePacket {
+		command.Env = append(command.Env, "GIT_TRACE_PACKET=1")
+	}
+
+	if g.trace {
+		command.Env = append(command.Env, "GIT_TRACE=1")
+	}
+
+	if g.curlVerbose {
+		command.Env = append(command.Env, "GIT_CURL_VERBOSE=1")
+	}
+
+	var stdOut strings.Builder
+	var stdErr strings.Builder
+
+	command.Stdout = &stdOut
+	command.Stderr = &stdErr
+
+	return command, &stdOut, &stdErr
+}
+
+func TestClone(t *testing.T) {
+	remoteRepoName := "test_repo.git"
+	clonedRepoName1 := "test_cloned_repo_1"
+	clonedRepoName2 := "test_cloned_repo_2"
+	fileContents := "This is a readme"
+
+	// Start server
+
+	// Router needed for handling the specific URL
+	router := GetRouter()
+
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+
+	cloneUrl := fmt.Sprintf("%s/repos/test/%s", ts.URL, remoteRepoName)
+
+	git := Git{
+		remoteName:  remoteRepoName,
+		cloneUrl:    cloneUrl,
+		tracePacket: testing.Verbose(),
+		trace:       testing.Verbose(),
+		curlVerbose: testing.Verbose(),
+	}
+
+	// Create a hosted repo
+	err := CreateRepo(remoteRepoName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer DeleteRepo(remoteRepoName)
+
+	// Git clone the repo elsewhere
+	fmt.Printf("cloneUrl: %v\n", cloneUrl)
+	command, stdOut, stdErr := git.Command(
+		"git",
+		"clone",
+		cloneUrl,
+		fmt.Sprintf("repositories/%s", clonedRepoName1),
+	)
+	err = command.Run()
+
+	log.Println("Git clone stdOut: ", stdOut.String())
+	log.Println("Git clone stdErr: ", stdErr.String())
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer DeleteRepo(clonedRepoName1)
+
+	// Create new file, add, commit and push to remote
+	err = os.WriteFile(fmt.Sprintf("repositories/%s/readme.md", clonedRepoName1), []byte(fileContents), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	command, stdOut, stdErr = git.Command(
+		"git",
+		"add",
+		".",
+	)
+	command.Dir = fmt.Sprintf("repositories/%s", clonedRepoName1)
+	err = command.Run()
+
+	log.Println("Git add stdOut: ", stdOut.String())
+	log.Println("Git add stdErr: ", stdErr.String())
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	command, stdOut, stdErr = git.Command(
+		"git",
+		"commit",
+		"-m",
+		"Initial commit",
+	)
+	command.Dir = fmt.Sprintf("repositories/%s", clonedRepoName1)
+	err = command.Run()
+
+	log.Println("Git commit stdOut: ", stdOut.String())
+	log.Println("Git commit stdErr: ", stdErr.String())
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	command, stdOut, stdErr = git.Command(
+		"git",
+		"push",
+	)
+	command.Dir = fmt.Sprintf("repositories/%s", clonedRepoName1)
+	err = command.Run()
+
+	log.Println("Git push stdOut: ", stdOut.String())
+	log.Println("Git push stdErr: ", stdErr.String())
+
+	if err != nil {
+		fmt.Println("Error during push")
+		t.Fatal(err)
+	}
+
+	// Clone repo elsewhere and verify contents
+
+	// Git clone the repo elsewhere
+	command, stdOut, stdErr = git.Command(
+		"git",
+		"clone",
+		cloneUrl,
+		fmt.Sprintf("repositories/%s", clonedRepoName2),
+	)
+	err = command.Run()
+
+	log.Println("Git clone2 stdOut: ", stdOut.String())
+	log.Println("Git clone2 stdErr: ", stdErr.String())
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer DeleteRepo(clonedRepoName2)
+
+	contents, err := os.ReadFile(fmt.Sprintf("repositories/%s/readme.md", clonedRepoName2))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(contents) != fileContents {
+		t.Fail()
+	}
+}
+
+func TestCreateRepo(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		repoName string
+		wantErr  bool
+	}{
+		{
+			repoName: "test",
+			wantErr:  true,
+		},
+		{
+			repoName: "test.git",
+			wantErr:  false,
+		},
+		{
+			repoName: "test repository with spaces.git",
+			wantErr:  true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if testing.Verbose() {
+				slog.SetLogLoggerLevel(slog.LevelDebug)
+			}
+			gotErr := CreateRepo(tt.repoName)
+			defer DeleteRepo(tt.repoName)
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("CreateRepo() failed: %v", gotErr)
+				}
+				return
+			}
+			if tt.wantErr {
+				t.Fatal("CreateRepo() succeeded unexpectedly")
+			}
+		})
+	}
+}
